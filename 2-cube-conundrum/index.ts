c39c30814ceaea9fb13045b7877d7225d76af2b8
Pablo Guerrero <pablo.gocv@gmail.com>
2023-12-04 18:33:21 +0000

    Cube conundrum part 1

---
+++ 2-cube-conundrum/index.ts
@@ -0,0 +1,65 @@
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+type Color = "red" | "green" | "blue";
+type Reveal = Record<Color, number>;
+
+type Game = {
+  id: number;
+  reveals: Reveal[];
+};
+
+const MAX_RED = 12;
+const MAX_GREEN = 13;
+const MAX_BLUE = 14;
+
+const games = parseGames();
+
+const possibleGames = games.filter((x) =>
+  x.reveals.every(
+    (r) => r.blue <= MAX_BLUE && r.green <= MAX_GREEN && r.red <= MAX_RED
+  )
+);
+
+const result = possibleGames.reduce((sum, nextGame) => sum + nextGame.id, 0);
+
+export function parseGames(): Game[] {
+  const fileContent = readFileSync(resolve(import.meta.dir, "./games.txt"), {
+    encoding: "utf-8",
+  });
+
+  const games: Game[] = [];
+
+  fileContent.split("\n").forEach((line, index) => {
+    const reveals = parseReveals(line);
+
+    games.push({
+      id: index + 1,
+      reveals,
+    });
+  });
+
+  return games;
+}
+
+function parseReveals(gameLine: string): Reveal[] {
+  const [_, reveals] = gameLine.split(":");
+
+  return reveals.split(";").map(parseReveal);
+}
+
+function parseReveal(revealString: string): Reveal {
+  const reveal: Reveal = {
+    red: 0,
+    blue: 0,
+    green: 0,
+  };
+
+  const reveals = revealString.split(",");
+  reveals.forEach((r) => {
+    const [amount, color] = r.trim().split(" ") as [string, Color];
+    reveal[color] = Number(amount);
+  });
+
+  return reveal;
+}
